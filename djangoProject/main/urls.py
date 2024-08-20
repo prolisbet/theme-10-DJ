32467fd4c4f31f5eb1eda9edb7869d6511cf28ae
prolisbet <lizatrushina96@gmail.com>
2024-08-20 14:11:04 +0300

    Django, lesson 2 code and homework

--- djangoProject/main/urls.py
+++ djangoProject/main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('new', views.new),
-    path('data', views.data),
-    path('test', views.test)
+    path('', views.index, name='home'),
+    path('new', views.new, name='page2'),
+    path('data', views.article, name='page3'),
+    path('test', views.test, name='page4')
 ]
--- djangoProject/main/views.py
+++ djangoProject/main/views.py
@@ -2,22 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+data = {
+        'caption': "ArtDjango"
+    }
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Это мой первый проект на Django</h1>')
+    return render(request, 'main/index.html', data)
 
 
 def new(request):
-    return HttpResponse('<h1>Это вторая страница моего проекта на Django</h1>')
+    return render(request, 'main/new.html', data)
 
 
-def data(request):
-    return HttpResponse('<h1>Это третья страница моего проекта на Django</h1>'
-                        '<hr>'
-                        '<h3>Здесь будут данные</h3>')
+def article(request):
+    return render(request, 'main/data.html')
 
 
 def test(request):
-    return HttpResponse('<h1>Это четвертая страница моего проекта на Django</h1>'
-                        '<hr>'
-                        '<h3>Здесь будут тесты</h3>')
+    return render(request, 'main/test.html')
