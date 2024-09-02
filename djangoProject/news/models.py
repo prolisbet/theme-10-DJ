4464b9edc12e6775e0ad4d9f0c954255c86c64a1
prolisbet <lizatrushina96@gmail.com>
2024-09-03 02:41:51 +0300

    django lesson 3

---
+++ djangoProject/news/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class News_post(models.Model):
+    title = models.CharField('Название новости', max_length=50)
+    short_description = models.CharField('Краткое описание новости', max_length=200)
+    text = models.TextField('Новость')
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    username = models.CharField('Имя пользователя', max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
